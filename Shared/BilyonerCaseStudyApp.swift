8f0b94aa85edb83573809b201ca133ff0cfe2770
Merve <mervegulact@gmail.com>
2021-09-02 23:57:19 +0300

    Create project

---
+++ Shared/BilyonerCaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BilyonerCaseStudyApp.swift
+//  Shared
+//
+//  Created by Merve on 2.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct BilyonerCaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Merve on 2.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+        }
+    }
+}
